b7d281c5bb8a49dc2eb3521bb3c79bf765b1d062
Keith <thechitsungo@gmail.com>
2026-05-14 15:08:34 +0200

    tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./public/src/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
